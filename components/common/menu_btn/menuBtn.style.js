9ae4bef75928fa865e5183cdcc0d282522053ba5
shroommm <khacnguyenquan@gmail.com>
2023-06-13 22:30:35 +0700

    BottomMenu, assets (icons)
    Adding more detail into the BottomMenu component and icons asset for later use

---
+++ components/common/menu_btn/menuBtn.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    width: 45,
+    height: 45,
+    backgroundColor: 'white',
+    borderRadius: 12 / 1.25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnImg: (dimension) => ({
+    width: dimension,
+    height: dimension,
+    borderRadius: 12 / 1.25,
+  }),
+});
+
+export default styles;
